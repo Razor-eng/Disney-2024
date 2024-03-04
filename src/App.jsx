a6a38354a80c672727ac67d33398e798ea433bef
Razor-eng <rajatmaharana64@gmail.com>
2024-03-04 22:07:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from './Componets/Header'
+import Slider from './Componets/Slider'
+import ProductionHouse from './Componets/ProductionHouse'
+import GenreMovieList from './Componets/GenreMovieList'
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Slider />
+
+      <ProductionHouse />
+
+      <GenreMovieList />
+    </div>
+  )
+}
+
+export default App
